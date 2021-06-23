e681a7d72a991d63c8d303c867d579b771365b58
N-eeraj <neeraj7rajeena@gmail.com>
2021-06-23 22:11:29 +0530

    dark mode for hello world page

--- page_script.js
+++ page_script.js
@@ -2,6 +2,9 @@ let darkMode = false;
 
 const navBar = document.getElementsByTagName("nav")[0];
 const darkBtn = document.getElementById("dark-btn");
+const description = document.getElementById("details");
+const terminal = document.getElementById("code");
+
 
 function toggle()
 {
@@ -11,11 +14,15 @@ function toggle()
         document.body.style.backgroundColor = '#111';
         navBar.className += 'dark-nav';
         darkBtn.setAttribute('src', "../Images/sun.png");
+        description.style.color = 'white';
+        terminal.style.backgroundColor = "#222";
     }
     else
     {
         document.body.style.backgroundColor = 'white';
         navBar.classList.remove("dark-nav")
         darkBtn.setAttribute('src', "../Images/moon.png");
+        description.style.color = 'black';
+        terminal.style.backgroundColor = "#111";
     }
 }
